1cc18e3ed172f31d1a4cf2f273d8cd868fb6eb94
Keg Avakian <kjavakian@outlook.com>
2023-09-19 15:27:48 -0500

    created a navigation bar with styling

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Icon from "../icon/Icon";
+import styles from "./navbar.module.css";
+
+export default function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.container}>
+        <span className={styles.iconContainer}>
+          <a className={styles.icon} href="#">
+            <Icon name="chevron-left" />
+            <Icon name="chevron-right" />
+          </a>
+        </span>
+        <div className={styles.linkContainer}>
+          <a className={styles.link} href="#about">
+            About
+          </a>
+          <a className={styles.link} href="#projects">
+            Projects
+          </a>
+          <a className={styles.link} href="#skills">
+            Skills
+          </a>
+          <a className={styles.link} href="#experience">
+            Experience
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+}
